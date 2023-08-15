76b240180c789532b5520ba147b25893ca07ef23
grififth <stevez9101@gmail.com>
2023-08-14 23:12:51 -0400

    complete first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import anime from "animejs";
-    let y = 0;
 
     let textAnimation;
 
@@ -9,42 +8,82 @@
         textAnimation = (function () {
             var mainText = document.querySelector(".main-text");
             var bgText = document.querySelector(".bg-text");
+            var registerButton = document.querySelector(".register-button");
 
             //main text rotate movement
             anime({
                 targets: mainText,
-                rotate: ["8deg", "0deg", "-8deg", "0deg"],
-                translateX: ["20px", "0px", "-20px", "0px"],
-                translateY: ["-20px", "-25px", "-10px", "-0px"],
-                duration: 10000,
-                easing: "easeInOutQuad",
+                rotate: ["3deg", "-3deg", "0deg"],
+                translateX: ["5px", "0px", "-5px", "0px"],
+                translateY: ["-5px", "-10px", "-15px", "-10px", "-5px", "-0px"],
+                duration: 5000,
+                easing: "linear",
                 loop: true,
             });
             //bg text rotate
             anime({
                 targets: bgText,
-                rotate: ["-5deg", "0deg", "5deg", "0deg"],
-                translateX: ["-10px", "0px", "10px", "0px"],
-                translateY: ["-20px", "-25px", "-10px", "-0px"],
-                duration: 15000,
-                easing: "easeInOutQuad",
+                rotate: ["-5deg", "5deg", "0deg"],
+                translateX: [
+                    "-10px",
+                    "-12px",
+                    "-7px",
+                    "0px",
+                    "7px",
+                    "10px",
+                    "12px",
+                    "10px",
+                    "0px",
+                ],
+                translateY: [
+                    "-10px",
+                    "-20px",
+                    "-25px",
+                    "-20px",
+                    "-10px",
+                    "-0px",
+                ],
+                duration: 3500,
+                easing: "linear",
+                loop: true,
+            });
+            //register button rotate
+            anime({
+                targets: registerButton,
+                rotate: ["-2deg", "2deg", "0deg"],
+                translateX: [
+                    "-10px",
+                    "-12px",
+                    "-7px",
+                    "0px",
+                    "7px",
+                    "10px",
+                    "12px",
+                    "10px",
+                    "0px",
+                ],
+                translateY: [
+                    "-10px",
+                    "-20px",
+                    "-25px",
+                    "-20px",
+                    "-10px",
+                    "-0px",
+                ],
+                duration: 10000,
+                easing: "linear",
                 loop: true,
             });
         })();
     });
 </script>
 
+<svelte:head>
+    <title>MC Hackathon 2023</title>
+    <meta name="description" content="MC Hackathon 2023" />
+</svelte:head>
+
 <div class="first-page" id="first-page">
-    <nav class={y === 0 ? "fadeOut" : "fadeIn"}>
-        <img src="logo.png" alt="" />
-        <div class="nav-links">
-            <a href="">About</a>
-            <a href="">Schedule</a>
-            <a href="">Sponsors</a>
-            <a href="">FAQ</a>
-            <a href="">Contact Us</a>
-        </div>
-    </nav>
     <div class="title-text">
         <div class="main-text">
             <h1 class="glitch one">Montgomery County</h1>
@@ -54,6 +93,9 @@
             <h1 class="glitch">2023</h1>
         </div>
     </div>
+    <button class="register-button hover-scale">
+        REGISTER
+    </button>
+    <div class="bg-fade" />
 </div>
-
-<svelte:window bind:scrollY={y} />
+<div class="second-page" />
