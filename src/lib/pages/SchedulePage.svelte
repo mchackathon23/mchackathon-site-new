27ee92bbb1e7cb1ab759a48e0baa5da16be451d6
grififth <stevez9101@gmail.com>
2023-08-21 21:20:39 -0400

    update schedule

--- src/lib/pages/SchedulePage.svelte
+++ src/lib/pages/SchedulePage.svelte
@@ -2,11 +2,11 @@
     <div class="schedule-page-content">
         <h1 class="glitch" id="schedule">Schedule</h1>
         <div class="schedule">
-            <p>8AM - 1PM: Opening ceremony and work time</p>
-            <p>1PM - 2PM: Hackathon concludes & judging</p>
-            <p>2PM - 3PM: Computer science trivia competition</p>
-            <p>3PM - 5PM: Escape room</p>
+            <p>8AM - 4PM: Opening ceremony and work time</p>
+            <p>4PM - 5PM: Hackathon concludes & judging</p>
             <p>5PM - 6PM: Closing ceremony</p>
+            <p>2PM - 4PM: Escape room (Optional)</p>
+            <p>4PM - 5PM: Computer science trivia competition (Optional)</p>
         </div>
     </div>
     <div class="bg-fade top" />
