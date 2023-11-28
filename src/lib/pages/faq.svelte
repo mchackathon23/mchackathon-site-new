92f7a87c9908645a252b7d06e25ea0b4ffa58a1b
tau315 <mc.hackathon23@gmail.com>
2023-11-28 18:19:35 -0500

    update

--- src/lib/pages/faq.svelte
+++ src/lib/pages/faq.svelte
@@ -11,6 +11,17 @@
 <style lang="scss">
     /* Your existing SCSS styles */
   
+    .faq-item {
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      padding: 10px;
+      margin-bottom: 10px;
+      cursor: pointer;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  
     .faq-answer {
       max-height: 0;
       overflow: hidden;
@@ -20,7 +31,7 @@
       font-size: 1.2rem;
       background-color: rgba(255, 255, 255, 0.1);
       padding: 10px;
-      border-radius: 1rem;
+      border-radius: 5px;
       margin-top: 5px;
     }
   
@@ -30,11 +41,19 @@
     }
   
     .question {
-      cursor: pointer;
       font-family: 'Orbitron', sans-serif;
       font-size: 1.5rem;
       color: #ffcc00;
-      margin-bottom: 10px;
+      margin-bottom: 0;
+    }
+  
+    .arrow {
+      font-size: 1.5rem;
+      transition: transform 0.3s ease;
+    }
+  
+    .arrow.down {
+      transform: rotate(180deg);
     }
   </style>
   
@@ -45,7 +64,14 @@
               {#each faqItems as item, index}
                 <div class="faq-item" on:click={() => toggleAnswer(index)}>
                   <div class="question">{item.question}</div>
-                  <div class="faq-answer" class:open={item.expanded}>{item.answer}</div>
+                  {#if item.expanded}
+                    <span class="arrow down">&#9650;</span>
+                  {:else}
+                    <span class="arrow">&#9658;</span>
+                  {/if}
+                  {#if item.expanded}
+                    <div class="faq-answer" class:open={item.expanded}>{item.answer}</div>
+                  {/if}
                 </div>
               {/each}
           </div>
