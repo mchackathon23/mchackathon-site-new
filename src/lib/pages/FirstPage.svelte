801ed7b45eb52c7f3bdbd5ed7d3e0abd46737c9d
tau315 <mc.hackathon23@gmail.com>
2023-11-28 19:03:40 -0500

    Update FirstPage.svelte

--- src/lib/pages/FirstPage.svelte
+++ src/lib/pages/FirstPage.svelte
@@ -32,4 +32,35 @@
     <video id="first-page-bg" preload="auto" autoplay loop muted>
         <source src="first-bg-compressed.mp4" />
     </video>
+    
+    <button
+    class="discord-button hover-scale"
+    on:click={() => {
+        window.open("https://discord.gg/JXZwNHeS");
+    }}
+>
+    DISCORD
+    </button>
 </div>
+
+<style lang="scss">
+    /* Existing SCSS styles */
+
+    .discord-button {
+        /* Add styles for Discord button */
+        background-color: #7289da; /* Discord's default color */
+        color: #fff; /* Text color */
+        padding: 10px 20px; /* Adjust padding as needed */
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px; /* Adjust font size */
+        margin-top: 20px; /* Add margin for spacing */
+        transition: background-color 0.3s ease, transform 0.3s ease;
+
+        &:hover {
+            background-color: #6277a2; /* Darker shade on hover */
+            transform: scale(1.05); /* Scale effect on hover */
+        }
+    }
+</style>
