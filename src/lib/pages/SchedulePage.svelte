ff0d6f5a65876db13702e5a615627b097000723c
Sterdycrab <152362155+Sterdycrab@users.noreply.github.com>
2023-11-29 08:23:14 -0500

    Update SchedulePage.svelte

--- src/lib/pages/SchedulePage.svelte
+++ src/lib/pages/SchedulePage.svelte
@@ -4,10 +4,10 @@
         <div class="schedule">
             <p>Arrival to Competition end (8:00 AM - 1:00 PM)</p>
             <p>Welcome presentation (1:00 PM - 1:15 PM)</p>
-            <p>Judging (1:45 PM - 3:30 PM)</p>
-            <p>Escape Room (3:45 PM - 4:45 PM)</p>
-            <p>Trivia (5:00 PM - 5:30 PM)</p>
-            <p>Closing ceremony (5:30 PM - 6:00 PM)</p>
+            <p>Judging (1:45 PM - 2:30 PM)</p>
+            <p>Escape Room (2:45 PM - 3:45 PM)</p>
+            <p>Trivia (4:00 PM - 4:30 PM)</p>
+            <p>Closing ceremony (4:30 PM - 5:00 PM)</p>
         </div>
     </div>
     <div class="bg-fade top" />
