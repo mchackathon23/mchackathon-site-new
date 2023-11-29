d12eed4184f4dfd63b117f1bcf830ab8ae13f786
Sterdycrab <152362155+Sterdycrab@users.noreply.github.com>
2023-11-29 00:03:37 -0500

    Update SchedulePage.svelte

--- src/lib/pages/SchedulePage.svelte
+++ src/lib/pages/SchedulePage.svelte
@@ -6,7 +6,7 @@
             <p>Welcome presentation (1:00 PM - 1:15 PM)</p>
             <p>Judging (1:45 PM - 3:30 PM)</p>
             <p>Escape Room (3:45 PM - 4:45 PM)</p>
-            <p>Trivia (5:00 PM - 5:30 PM)</p>
+            <p>Trivia (5:00 PM -  5:30 PM)</p>
             <p>Closing ceremony (5:30 PM - 6:00 PM)</p>
         </div>
     </div>
