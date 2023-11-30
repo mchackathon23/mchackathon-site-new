3fb6352341df036e6e3df12bd1f6dc3a8f3c57c3
mchackathon <138955885+mchackathon@users.noreply.github.com>
2023-11-30 09:44:56 -0500

    Update SchedulePage.svelte

--- src/lib/pages/SchedulePage.svelte
+++ src/lib/pages/SchedulePage.svelte
@@ -2,7 +2,7 @@
     <div class="schedule-page-content">
         <h1 class="glitch" id="schedule">Schedule</h1>
         <div class="schedule">
-            <p>Arrival to Competition end (8:00 AM - 1:00 PM)</p>
+            <p>Arrival to Competition end (8:00 AM - 1:00 PM) </p>
             <p>Welcome presentation (1:00 PM - 1:15 PM)</p>
             <p>Judging (1:45 PM - 2:30 PM)</p>
             <p>Escape Room (2:45 PM - 3:45 PM)</p>
